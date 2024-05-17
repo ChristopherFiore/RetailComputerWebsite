01b1896331cf3333a24a8b9ffea569ffde90bfd0
Christopher Fiore <chrisfiore292@gmail.com>
2024-05-17 13:00:25 +1000

    ALOT OF DESGIN IMPLEMENTED NOW

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 // ---COUNT WHEN ADDING TO CART--
 var itemCount = 0;
-document.getElementById("counter").textContent=itemCount;
 
 function IncrementCartCount() {
     itemCount = itemCount + 1;
-    UpdateCountDisplay(); 
+    UpdateCountDisplay();
 }
 
 function UpdateCountDisplay() {
-    document.getElementById("counter").innerHTML=itemCount;
+    document.getElementById("counter").innerHTML = itemCount;
 }
+
+// ---CODE FOR FLEX DIRECTION WHEN RESIZING
+window.addEventListener('resize', function(event){
+    // THIS WORKS
+    var screenWidth = window.innerWidth;
+    if (screenWidth < parseInt("800px", 10)) {
+        document.getElementById("pc").style.flexDirection = "column";
+        //document.body.style.background = 'red';
+    } else {
+        document.getElementById("pc").style.flexDirection = "row";
+    }
+});
