bb0090ab88a01a8cfd90ff96de9152c0bc31b138
Christopher Fiore <chrisfiore292@gmail.com>
2024-05-17 13:22:09 +1000

    JS IS WORKING

--- script.js
+++ script.js
@@ -12,11 +12,19 @@ function UpdateCountDisplay() {
 
 // ---CODE FOR FLEX DIRECTION WHEN RESIZING
 window.addEventListener('resize', function(event){
-    // THIS WORKS
+    //  Variables
     var screenWidth = window.innerWidth;
+    var leftColumn = document.getElementById("pcLeft");
+    var rightColumn = document.getElementById("pcRight");
+    var leftBorder = document.getElementById("leftBorder");
+    var rightBorder = document.getElementById("rightBorder");
+
+    // Functionality
     if (screenWidth < parseInt("800px", 10)) {
         document.getElementById("pc").style.flexDirection = "column";
-        //document.body.style.background = 'red';
+        leftColumn.style.width = "60vw";
+        leftBorder.style.width = "20vw";
+        rightBorder.style.width = "20vw";
     } else {
         document.getElementById("pc").style.flexDirection = "row";
     }
